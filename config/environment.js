eacf51ec45dd027b9c5a30959f46580ec9797b42
sainth-stack <123sainathreddy100@gmail.com>
2026-07-21 17:21:59 +0530

    updated env

--- config/environment.js
+++ config/environment.js
@@ -1,12 +1,12 @@
 const PORT = 4000
 
-const DATABASE_URL = `mongodb+srv://prasanth:Sai%4012345@cluster0.gmxleae.mongodb.net/talent-spotify-ai?retryWrites=true&w=majority`
+const DATABASE_URL = `mongodb+srv://prasanth:Sai%4012345@cluster0.gmxleae.mongodb.net/vihanga-prod?retryWrites=true&w=majority`
 //const DATABASE_URL = "mongodb://localhost:27017/talentSpotifyDB"
 
 const JWT_SECRET = "$2a$12$e9HMflla.nPm9t8LOopEdeTYD.fmAYbxQ4p6XbLNylKRgcDEMXPba"
   // const CLIENTURL="http://localhost:4300";
 // const CLIENTURL="https://talentspotifyapp.com"
-const CLIENTURL="https://ai.talentspotifyapp.com"
+const CLIENTURL="https://vihanga.talentspotifyapp.com"
 
 // Psychometric assessment now lives inside the main frontend app
 const PhsychometricURL=`${CLIENTURL}/psychometric-test`;
