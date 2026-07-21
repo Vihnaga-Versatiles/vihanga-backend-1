e26c27c3ee0349ac5af6e68da85aca3b1ec738fa
sainth-stack <123sainathreddy100@gmail.com>
2026-07-21 18:29:22 +0530

    fixed

--- config/environment.js
+++ config/environment.js
@@ -6,7 +6,7 @@ const DATABASE_URL = `mongodb+srv://prasanth:Sai%4012345@cluster0.gmxleae.mongod
 const JWT_SECRET = "$2a$12$e9HMflla.nPm9t8LOopEdeTYD.fmAYbxQ4p6XbLNylKRgcDEMXPba"
   // const CLIENTURL="http://localhost:4300";
 // const CLIENTURL="https://talentspotifyapp.com"
-const CLIENTURL="https://vihanga.talentspotifyapp.com"
+const CLIENTURL="https://stage.talentspotify.com"
 
 // Psychometric assessment now lives inside the main frontend app
 const PhsychometricURL=`${CLIENTURL}/psychometric-test`;
