dfaa81f79e7ebd4011ef2e1b5108341b21aaf62a
sainth-stack <123sainathreddy100@gmail.com>
2026-07-28 18:07:58 +0530

    updated env

--- config/environment.js
+++ config/environment.js
@@ -1,6 +1,7 @@
 const PORT = 4000
 
-const DATABASE_URL = `mongodb+srv://prasanth:Sai%4012345@cluster0.gmxleae.mongodb.net/vihanga-prod?retryWrites=true&w=majority`
+// const DATABASE_URL = `mongodb+srv://prasanth:Sai%4012345@cluster0.gmxleae.mongodb.net/vihanga-prod?retryWrites=true&w=majority`
+const DATABASE_URL = `mongodb+srv://admin_db_user:7032031366@Aa@hr-vihanga.wzt14u.mongodb.net/pre-prod?retryWrites=true&w=majority`
 //const DATABASE_URL = "mongodb://localhost:27017/talentSpotifyDB"
 
 const JWT_SECRET = "$2a$12$e9HMflla.nPm9t8LOopEdeTYD.fmAYbxQ4p6XbLNylKRgcDEMXPba"
